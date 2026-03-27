8616962d1571bd9277616ac81d7ad47fb8c0efa8
abdallammdouh22-spec <abdalla.mmdouh22@gmail.com>
2026-03-27 01:01:13 -0700

    Update modelsh
    update the model

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -e
-
 chown -R openclaw:openclaw /data
 chmod 700 /data
-
 if [ ! -d /data/.linuxbrew ]; then
   cp -a /home/linuxbrew/.linuxbrew /data/.linuxbrew
 fi
-
 rm -rf /home/linuxbrew/.linuxbrew
 ln -sfn /data/.linuxbrew /home/linuxbrew/.linuxbrew
-
+openclaw config set defaultModel nvidia/nemotron-3-super-120b-a12b:free
 exec gosu openclaw node src/server.js
